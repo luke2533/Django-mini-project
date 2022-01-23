a96b59fb2c0f99fc140eaa606a9895e807ab860b
luke2533 <lukefoxwell1@gmail.com>
2022-01-23 23:32:50 +0000

    Deleting Products

--- products/urls.py
+++ products/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     # int: is used so that django doesn't mistake add/ for a product ID
     path('add/', views.add_product, name="add_product"),
     path('edit/<int:product_id>/', views.edit_product, name="edit_product"),
+    path('delete/<int:product_id>/', views.delete_product, name="delete_product"),
 ]
