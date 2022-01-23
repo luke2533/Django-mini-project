8811d760d3646c3e1d1de35c5ebdd650825fb595
luke2533 <lukefoxwell1@gmail.com>
2022-01-23 23:12:32 +0000

    Add product view, url and template

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name="products"),
-    path('<product_id>', views.product_detail, name="product_detail"),
+    path('<int:product_id>/', views.product_detail, name="product_detail"),
+    # int: is used so that django doesn't mistake add/ for a product ID
+    path('add/', views.add_product, name="add_product"),
 ]
